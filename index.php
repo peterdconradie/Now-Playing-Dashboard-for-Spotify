29b8bd64467ef12a91e106d4f021f1197e2da784
pconradi <Peter.Conradie@UGent.be>
2020-02-18 21:08:01 +0100

    fixed typo

--- index.php
+++ index.php
@@ -25,15 +25,15 @@ if(isset($_GET['lang'])){
 <body>
 <div id="all_content">
 <div id="playing-div">
-  <h1>Npw Playing Dashboard for Spotify</h1>
+
     <div id="left-side"></div>
 
     <div id="main">
-            <h1>Welcome</h1>
+            <h1>Now Playing Dashboard for Spotify</h1>
 
 
-            <a href="login.php" class="spotify-btn"><p>Connect to your Spotify Account</p></a>
-            <p class="space20"></p>
+            <a href="login.php" class="spotify-btn"><h1>Connect to your Spotify Account</h1></a>
+            
             <h3>Credits</h3>
             <p>This is an opensource project. Is is derived for the most part on <a href="https://github.com/busybox11/NowPlaying-for-Spotify">busybox11's NowPlaying for Spotify</a>,
            which also relies on the <a href="https://github.com/JMPerez/spotify-web-api-js">spotify-web-api-js</a> by José M. Pérez. The code can be accessed on <a href="https://github.com/peterdconradie/Now-Playing-Dashboard-for-Spotify/">GitHub</a></p>
